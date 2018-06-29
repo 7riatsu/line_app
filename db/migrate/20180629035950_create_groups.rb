8c16deba61cbcae105d3543e8064bf4bef703ed4
nariatsu1202 <atsuki02jojo@gmail.com>
2018-06-29 13:08:40 +0900

    add index to users and groups

--- db/migrate/20180629035950_create_groups.rb
+++ db/migrate/20180629035950_create_groups.rb
@@ -1,8 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false, index: true, unique: true
       t.timestamps
+      add_index :groups, :name
     end
   end
 end
