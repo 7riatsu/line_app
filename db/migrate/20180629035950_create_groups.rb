962e39a746ef88f1fc862a668fd9dfe9e6152a31
nariatsu1202 <atsuki02jojo@gmail.com>
2018-06-29 13:12:04 +0900

    migrate

--- db/migrate/20180629035950_create_groups.rb
+++ db/migrate/20180629035950_create_groups.rb
@@ -3,7 +3,6 @@ class CreateGroups < ActiveRecord::Migration[5.1]
     create_table :groups do |t|
       t.string :name, null: false, index: true, unique: true
       t.timestamps
-      add_index :groups, :name
     end
   end
 end
